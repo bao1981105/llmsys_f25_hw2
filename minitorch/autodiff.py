2b6c67a9805a26be656e40816dbe13aaa72d8358
xuhan <hannah.han@bytedance.com>
2025-09-13 23:03:35 -0700

    problem1 first commit

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -102,8 +102,21 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     """
     # BEGIN ASSIGN1_1
     # TODO
-    
-    raise NotImplementedError("Task Autodiff Not Implemented Yet")
+    # parents of variable means the inputs needed to calculate this variable result
+    visited = set()
+    res = []
+
+    def dfs(variable: Variable) -> None:
+        if variable.is_constant() or variable.unique_id in visited:
+            return
+        visited.add(variable.unique_id)
+        for input in variable.parents:
+            dfs(input)
+        res.append(variable)
+    dfs(variable)
+    # res vector stores the variables(tensors) from input to output, forward pass topological order
+    # res[::-1] is backward propagation order, we process output first
+    return res[::-1]
     # END ASSIGN1_1
 
 
@@ -118,10 +131,48 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
+
+    # Example: Visualization
+    # a(leaf) - -exp --> c \
+    #                    * --> e
+    # b(leaf) - -inv --> d /
+
+    # # Example Forward pass
+    # a = Tensor.make([2.0], (1,), backend=backend)
+    # b = Tensor.make([3.0], (1,), backend=backend)
+    # c = a.exp()  # c = exp(a)
+    # d = 1 / b  # d = inv(b)
+    # e = c * d  # e = c * d
+    # # Example Backward pass
+    # e.backward()  # triggers backpropagate(e, Tensor([1.0]))
+    # # During backpropagation:
+    # # e.chain_rule(Tensor([1.0])) -> [(c, Tensor([1.0]) * d), (d, Tensor([1.0]) * c)]
+    # # c.chain_rule(Tensor([1.0]) * d) -> [(a, (Tensor([1.0]) * d) * exp(a))]
+    # # d.chain_rule(Tensor([1.0]) * c) -> [(b, (Tensor([1.0]) * c) * (-1 / b^2))]
+
+
     # BEGIN ASSIGN1_1
-    # TODO
-   
-    raise NotImplementedError("Task Autodiff Not Implemented Yet")
+    backward_topo_order = topological_sort(variable)
+    grad_map = {variable.unique_id: deriv}
+    for v in backward_topo_order:
+        if v.is_constant():
+            continue
+        grad = grad_map.get(v.unique_id, None)
+        if grad is None:
+            continue
+        # is_leaf: it was created directly by the user and not as the result of a differentiable operation
+        # most operations are differentiable in our discussion scope
+        if v.is_leaf():
+            v.accumulate_derivative(grad)
+        else:
+            # v.chain_rule returns (input1 of v, ∂loss/∂(input1 of v)), the argument to v.chain_rule grad = ∂loss/∂v
+            # basically, v.chain_rule appends ∂v/∂(input1 of v) to grad(=∂loss/∂v)
+            for parent, parent_grad in v.chain_rule(grad):
+                if parent.unique_id in grad_map:
+                    grad_map[parent.unique_id] += parent_grad
+                else:
+                    grad_map[parent.unique_id] = parent_grad
+
     # END ASSIGN1_1
 
 
